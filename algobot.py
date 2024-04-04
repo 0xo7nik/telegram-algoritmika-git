a31bf55b7eac60f458a084e912bad50014d943cf
Михаил <tsymisha@gmail.com>
2024-04-04 15:53:58 +0300

    Создал бот в тг

---
+++ algobot.py
@@ -0,0 +1,18 @@
+import telebot
+import json
+
+def get_token():
+    token = ''
+    with open('token.json') as file:
+        json_answer = json.load(file)
+        token = json_answer['token']
+    return token
+
+token = get_token()
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["start"])
+def main(message):
+    bot.send_message(message.from_user.id, "Привет, я бот")
+
+bot.polling(none_stop=True, interval=0, timeout=120)
