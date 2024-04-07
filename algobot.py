a6689d483bfe5ef27ff5d9d63a48b511bc03c0ce
algo <algo@algo.algo>
2024-04-07 17:45:43 +0300

    just commit

---
+++ algobot.py
@@ -0,0 +1,34 @@
+import telebot
+import json
+
+def get_token():
+    token = ''
+    with open('token.json') as file:
+        json_answer = json.load(file)
+        token = json_answer['config']
+    return token
+
+token = get_token()
+bot = telebot.TeleBot(token)
+
+#@bot.message_handler(commands=["start"])
+#def main(message):
+#    bot.send_message(message.from_user.id, "Привет, чем могу помочь?")
+
+
+@bot.message_handler(commands=["start"])
+def main(message):
+    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    
+privet = "привет"
+privet = privet.lower()
+
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+def get_text_messages(message):
+    if message.text == privet:
+        bot.send_message(message.from_user.id, "QQ")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0, timeout=120)
