d695ddeee0cb6c785d6ba44f88f50e8f43b722fa
Михаил <tsymisha@gmail.com>
2024-04-08 20:40:05 +0300

    just commit32

--- algobot.py
+++ algobot.py
@@ -19,13 +19,10 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=["start"])
 def main(message):
     bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    
-privet = "привет"
-privet = privet.lower()
 
 @bot.message_handler(content_types=['text', 'document', 'audio'])
 def get_text_messages(message):
-    if message.text == privet:
+    if message.text.lower() == "привет":
         bot.send_message(message.from_user.id, "QQ")
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши привет")
