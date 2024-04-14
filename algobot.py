2a21afa2b028265156091e3ca994859cbaea6f95
Михаил <tsymisha@gmail.com>
2024-04-14 19:37:06 +0300

    upd my algobot

--- algobot.py
+++ algobot.py
@@ -11,24 +11,25 @@ def get_token():
 token = get_token()
 bot = telebot.TeleBot(token)
 
-#@bot.message_handler(commands=["start"])
-#def main(message):
-#    bot.send_message(message.from_user.id, "Привет, чем могу помочь?")
+@bot.message_handler(commands=["start", "help"])
+def send_start(message):
+    if message.text.lower() == "/start":
+        bot.send_message(message.from_user.id, "Привет! Я могу помочь тебе с любым математическим выражением.")
+    if message.text.lower() == "/help":
+        bot.send_message(message.from_user.id, "Напиши выражение, которое надо решить")
 
+@bot.message_handler(content_types=['text'])
+def send_answer(message):
+    if message.text.lower():
+        bot.send_message(message.from_user.id, "1")
 
-@bot.message_handler(commands=["start"])
-def main(message):
-    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-
-privet = "привет"
-privet = privet.lower()
-
-@bot.message_handler(content_types=['text', 'document', 'audio'])
-def get_text_messages(message):
-    if message.text.lower() == "привет":
-        bot.send_message(message.from_user.id, "QQ")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 bot.polling(none_stop=True, interval=0, timeout=120)
+
+
+@bot.message_handler(func=lambda message: True)
+def calc(message): 
+#    try: 
+#        result = parser.parse(message.text)
+#        bot.send_message(message.chat.id, str(result)) 
+#    except:
+    pass
