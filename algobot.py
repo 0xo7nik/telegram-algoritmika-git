bb61a9a7aabf4d9b5a811bb193acf7bd50a79d14
Михаил <tsymisha@gmail.com>
2024-05-27 18:50:11 +0300

    upd bot

--- algobot.py
+++ algobot.py
@@ -18,43 +18,62 @@ bot = telebot.TeleBot(token)
 length, height = 0, 0
 @bot.message_handler(commands=["start", "maths", "triangle_area", "circle_area"])
 def send_start(message):
+    def get_length(message):
+        global length, height
+        length = int(message.text)
+        if message.text:
+            height = bot.send_message(message.from_user.id, "Высота треугольника?")
+            bot.register_next_step_handler(height, send_answer)
+        else:
+            bot.send_message(message.from_user.id, "Я тебя не понимаю.")
+            
+    def send_answer(message):
+        global length, height
+        height = int(message.text)
+        if message.text:
+            bot.send_message(message.from_user.id, f"Площадь треугольника равна: {(height*length)/2}")
+        else:
+            bot.send_message(message.from_user.id, "Я тебя не понимаю.")
+
+    def get_maths(message):
+        global maths
+        maths = message.text
+        if message.text:
+            bot.send_message(message.from_user.id, f"Ответ: {eval(message.text)}")
+        else:
+            bot.send_message(message.from_user.id, "Я тебя не понимаю.")
+
+    def get_radius(message):
+        global radius
+        radius = int(message.text)
+        if message.text == radius:
+            bot.send_message(message.from_user.id, f"Площадь круга равна: {math.pi*(int(message.text)**2)}")
+        else:
+            bot.send_message(message.from_user.id, "Я тебя не понимаю.")
+
     if message.text.lower() == "/start":
-        bot.send_message(message.from_user.id, "Привет! Я могу помочь тебе посчитать площадь треугольника по команде /area, посчитать любое математическое выражение по команде /maths, а также посчитать площадь круга по команде /circle_area.")
+        bot.send_message(message.from_user.id, "Привет! Я могу помочь тебе посчитать площадь треугольника по команде /triangle_area, посчитать любое математическое выражение по команде /maths, а также посчитать площадь круга по команде /circle_area.")
         users[message.from_user.id] = {
             'action': 'start',
             'data': []
         }
-    if message.text.lower() == "/triangle_area":
+    elif message.text.lower() == "/triangle_area":
         length = bot.send_message(message.from_user.id, "Длина основания треугольника?")
         bot.register_next_step_handler(length, get_length)
-    if message.text.lower() == "/maths":
+
+    elif message.text.lower() == "/maths":
         maths = bot.send_message(message.from_user.id, "Напиши любое выражение")
-        bot.register_next_step_handler(maths, get_maths)
-    if message.text.lower() == "/circle_area":
+        if int(message.text):
+            bot.register_next_step_handler(maths, get_maths)
+        else:
+            bot.send_message(message.from_user.id, "Я тебя не понимаю.")
+
+    elif message.text.lower() == "/circle_area":
         radius = bot.send_message(message.from_user.id, "Радиус окружности?")
         bot.register_next_step_handler(radius, get_radius)
 
-#triangle_area
-def get_length(message):
-    global length, height
-    length = int(message.text)
-    height = bot.send_message(message.from_user.id, "Высота треугольника?")
-    bot.register_next_step_handler(height, send_answer)
-def send_answer(message):
-    global length, height
-    height = int(message.text)
-    bot.send_message(message.from_user.id, f"Площадь треугольника равна: {(height*length)/2}")
-
-#maths
-def get_maths(message):
-    global maths
-    maths = message.text
-    bot.send_message(message.from_user.id, eval(message.text))
-
-#circle_area
-def get_radius(message):
-    global radius
-    radius = int(message.text)
-    bot.send_message(message.from_user.id, f"Площадь круга равна: {math.pi*(radius*radius)}")
-
+@bot.message_handler(content_types=["text"])
+def send_text(message):
+    if message.text:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Все мои команды указаны в /start.")
 bot.polling(none_stop=True, interval=0, timeout=120)
